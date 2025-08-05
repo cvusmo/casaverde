c8b182cc92aef6c422ad7b008dfd576239e10b5f
cvusmo <blacksheepcosmo@gmail.com>
2025-08-05 10:41:08 -0500

    Changed test logic.

--- casaverde_controller/src/controller.rs
+++ casaverde_controller/src/controller.rs
@@ -33,14 +33,14 @@ pub fn process_local_readings(local_temp: Option<f32>, controller_id: &str) -> V
             info!(
                 "Local temperature above 40°C for {local_device_id} on {controller_id}: {temp}. Turning on INT1 (Red LED)."
             );
-            commands.push(Command::TurnOnCooling("INT1".to_string())); // Red LED
-            commands.push(Command::TurnOffCooling("INT2".to_string())); // Blue LED
+            commands.push(Command::TurnOnCooling("INT1".to_string()));
+            commands.push(Command::TurnOffCooling("INT2".to_string()));
         } else if temp < 39.0 {
             info!(
                 "Local temperature below 39°C for {local_device_id} on {controller_id}: {temp}. Turning on INT2 (Blue LED)."
             );
-            commands.push(Command::TurnOffCooling("INT1".to_string())); // Red LED
-            commands.push(Command::TurnOnCooling("INT2".to_string())); // Blue LED
+            commands.push(Command::TurnOffCooling("INT1".to_string()));
+            commands.push(Command::TurnOnCooling("INT2".to_string()));
         } else {
             info!(
                 "Local temperature stable for {local_device_id} on {controller_id}: {temp}. Both OFF."
@@ -49,20 +49,18 @@ pub fn process_local_readings(local_temp: Option<f32>, controller_id: &str) -> V
             commands.push(Command::TurnOffCooling("INT2".to_string()));
         }
     } else {
-        // TESTMODE: no sensor is wired
+        // Testing mode since no sensor is wired
         info!("No temperature sensor detected for {controller_id}. Testing mode:");
-        info!("TEST OFF - Simulating both LEDs off");
-        commands.push(Command::TurnOffCooling("INT1".to_string()));
-        commands.push(Command::TurnOffCooling("INT2".to_string()));
-
         static mut CYCLE: bool = false;
         unsafe {
             if CYCLE {
                 info!("TEST ON - Simulating INT1 (Red LED) on");
                 commands.push(Command::TurnOnCooling("INT1".to_string()));
-            } else {
-                info!("TEST OFF - Simulating INT2 (Blue LED) off");
                 commands.push(Command::TurnOffCooling("INT2".to_string()));
+            } else {
+                info!("TEST ON - Simulating INT2 (Blue LED) on");
+                commands.push(Command::TurnOffCooling("INT1".to_string()));
+                commands.push(Command::TurnOnCooling("INT2".to_string()));
             }
             CYCLE = !CYCLE;
         }
